ba35dc6eedb94cb78ad6a119549fbb80df80ec59
ZhangQiangQQ <1562055324@qq.com>
2017-09-22 12:28:43 +0800

    add folder

---
+++ vue-music/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    Benjour 
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style  lang="stylus">
+@import"common/stylus/variable"
+
+#app 
+  color: $color-theme
+</style>
