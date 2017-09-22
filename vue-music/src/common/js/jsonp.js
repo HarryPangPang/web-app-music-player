39cb969888397a35cd016eeeb0bc1ee268d002b0
ZhangQiangQQ <1562055324@qq.com>
2017-09-22 15:01:59 +0800

    add jsonp

---
+++ vue-music/src/common/js/jsonp.js
@@ -0,0 +1,25 @@
+// 二次封装jsonp
+import originJsonp from 'jsonp'
+export default function jsonp (url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+  return new Promise((resolve, reject) => {
+    originJsonp(url, option, (err, data) => {
+      if (!err) {
+        // 成功抛出数据
+        resolve(data)
+      } else {
+        // 失败抛出错误
+        reject(err)
+      }
+    })
+  })
+  function param (data) {
+    let url = ''
+    for (var k in data) {
+      let value
+      value !== undefined ? data[k] : ''
+      url += `&${k}=${encodeURIComponent(value)}`
+    }
+    return url ? url.substring(1) : ''
+  }
+}
