0cdd7be49126ea62f97a51a40e6940fd71a77f1a
ZhangQiangQQ <1562055324@qq.com>
2017-09-22 14:03:12 +0800

    add header

--- vue-music/src/App.vue
+++ vue-music/src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
-    Benjour 
+    <Mheader></Mheader>
+    <Tab></Tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+// 引入组件
+import Mheader from './components/m-header/m-header'
+import Tab from './components/tab/tab'
 
+// 导出注册组件
+export default {
+  components: {
+    Mheader,
+    Tab
+  }
+}
 </script>
 
 <style  lang="stylus">
-@import"common/stylus/variable"
-
-#app 
-  color: $color-theme
 </style>
--- vue-music/src/router/index.js
+++ vue-music/src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import rank from '../components/rank/rank.vue'
+import recommend from '../components/recommend/recommend.vue'
+import search from '../components/search/search.vue'
+import singer from '../components/singer/singer.vue'
 
 Vue.use(Router)
-
+// 路由配置
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      redirect: '/recommend'
+      // 利用router的redirect属性来重定向根目录指定路径
+    },
+    {
+      path: '/rank',
+      component: rank
+    },
+    {
+      path: '/recommend',
+      component: recommend
+    },
+    {
+      path: '/search',
+      component: search
+    },
+    {
+      path: '/singer',
+      component: singer
     }
   ]
 })
